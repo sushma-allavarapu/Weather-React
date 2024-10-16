13ce36f8694000f9ab081c91a79ccd2a8d88d511
sushma-allavarapu <144252604+sushma-allavarapu@users.noreply.github.com>
2024-10-16 20:58:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import { useEffect } from "react";
-import Card from "./components/Card";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import { useWeather } from "./context/Weather";
+import React,{useState} from 'react'
 
-import "./App.css";
-
-function App() {
-  const weather = useWeather();
-  console.log(weather);
-
-  useEffect(() => {
-    // Get Current Location
-    weather.fetchCurrentUserLocationData();
-  }, []);
+const App = () => {
+  
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+  const changeHandler = e =>{
+    setCity(e.target.value);
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
+  }
 
   return (
-    <div className="App">
-      <h1>Weather Forecast</h1>
-      <Input />
-      <Button onClick={weather.fetchData} value="Search" />
-      <Card />
-      <Button value="Refresh" />
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather App</h4>
+            <form onSubmit={submitHandler}>
+              <input size="30" type="text" name="city" onChange={changeHandler} value={city}/> <br /><br />
+              <input type="submit" value="Get Temperature" />
+            </form><br /> <br />
+            <div>
+               <h1>{result}</h1> 
+            </div>
+          </div>
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
